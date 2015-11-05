e1984ce0ff45ce73acea23ad266069616bd81bd3
Margarida Reis <margarida.reis@ist.utl.pt>
2015-11-05 11:21:50 +0000

    fixed an issue with table creation - it's now working. need to review datatypes

--- part2/table creation.sql
+++ part2/table creation.sql
@@ -1,108 +1,101 @@
-drop table if exists Patient;
-drop table if exists PAN;
-drop table if exists Device;
-drop table if exists Sensor;
-drop table if exists Actuator;
-drop table if exists Municipality;
-drop table if exists Period;
-drop table if exists Reading;
-drop table if exists Setting;
 drop table if exists Wears;
 drop table if exists Lives;
 drop table if exists Connects;
+drop table if exists Reading;
+drop table if exists Setting;
+drop table if exists Actuator;
+drop table if exists Sensor;
+drop table if exists Patient;
+drop table if exists PAN;
+drop table if exists Device;
+drop table if exists Municipality;
+drop table if exists Period;
+
 
 create table Patient
-   (number  ,
+   (number  numeric(9,0), /* health no. in portugal has 9 digits */
     name    varchar(255),
-    address varchar(255),
+    address	varchar(255),
     primary key(number));
 
 create table PAN
-   (domain  varchar(255),
-    phone   ,
+   (domain	varchar(255),
+    phone	  numeric(9,0),  /* phones numbers in portugal have 9 digits */
     primary key(domain));
 
 create table Device
-   (serialnum     ,
-    manufacturer  varchar(255),
-    description   varchar(255),
+   (serialnum     NUMERIC(8,0), /* how many digits has the serial number of a device? */
+    manufacturer  VARCHAR(255),
+    description		VARCHAR(255),
     primary key(serialnum, manufacturer));
 
 create table Sensor
-   (snum     ,
-    manuf varchar(255),
-    units varchar(255),
+   (snum	NUMERIC(8,0),
+    manuf	VARCHAR(255),
+    units	VARCHAR(255),
     primary key(snum, manuf),
-    foreign key(snum) references Device(serialnum)
-    foreign key(manuf) references Device(manufacturer));
+    foreign key(snum, manuf) references Device(serialnum, manufacturer));
 
 create table Actuator
-   (snum     ,
-    manuf varchar(255),
-    units varchar(255),
+   (snum	NUMERIC(8,0),
+    manuf	VARCHAR(255),
+    units	VARCHAR(255),
     primary key(snum, manuf),
-    foreign key(snum) references Device(serialnum)
-    foreign key(manuf) references Device(manufacturer));
+    foreign key(snum, manuf) references Device(serialnum, manufacturer));
 
 create table Municipality
-   (nut4code     ,
-    name      varchar(255),
+   (nut4code	NUMERIC(5,0), /* 5 digit code assigned by the National Bureau of Statistics */
+    name		  VARCHAR(255),
     primary key(nut4code));
 
 create table Period
-   (start     ,
-    end       ,
+   (start	DATETIME,
+    end		DATETIME,
     primary key(start, end));
 
 create table Reading
-   (snum     ,
-    manuf     varchar(255),
-    datetime  DATETIME
-    value
+   (snum		  NUMERIC(8,0),
+    manuf     VARCHAR(255),
+    datetime  DATETIME,
+    value     NUMERIC(5,2),  /* values read from the sensors have 5 total digits and 2 fractional digits */
     primary key(snum, manuf, datetime),
-    foreign key(snum) references Sensor(snum)
-    foreign key(manuf) references Sensor(manuf));
+    foreign key(snum, manuf) references Sensor(snum, manuf));
 
 create table Setting
-   (snum     ,
-    manuf     varchar(255),
-    datetime  DATETIME
-    value
+   (snum		  NUMERIC(8,0),
+    manuf		  VARCHAR(255),
+    datetime  DATETIME,
+    value		  NUMERIC(5,2), /* settings sent to the actuators have 5 total digits and 2 fractional digits */
     primary key(snum, manuf, datetime),
-    foreign key(snum) references Actuator(snum)
-    foreign key(manuf) references Actuator(manuf));
+    foreign key(snum, manuf) references Actuator(snum, manuf));
 
 create table Wears
-   (start     ,
-    end    	varchar(255),
-    patient
-    pan
+   (start     DATETIME,
+    end    	  DATETIME,
+    patient   NUMERIC(9,0),
+    pan       VARCHAR(255),
     primary key(start, end, patient),
-    foreign key(start) references Period(start),
-    foreign key(end) references Period(end),
+    foreign key(start, end) references Period(start, end),
     foreign key(patient) references Patient(number),
     foreign key(pan) references PAN(domain));
 
 create table Lives
-   (start     ,
-    end    	varchar(255),
-    patient
-    muni
+   (start   DATETIME,
+    end    	DATETIME,
+    patient	NUMERIC(9,0),
+    muni    NUMERIC(5,0),
     primary key(start, end, patient),
-    foreign key(start) references Period(start),
-    foreign key(end) references Period(end),
+    foreign key(start, end) references Period(start, end),
     foreign key(patient) references Patient(number),
     foreign key(muni) references Municipality(nut4code));
 
 create table Connects
-   (start     ,
-    end    	varchar(255),
-    snum
-    manuf
-    pan
+   (start   DATETIME,
+    end    	DATETIME,
+    snum	  NUMERIC(8,0),
+    manuf   VARCHAR(255),
+    pan     VARCHAR(255),
     primary key(start, end, snum, manuf),
-    foreign key(start) references Period(start),
-    foreign key(end) references Period(end),
-    foreign key(snum) references Device(serialnum),
-    foreign key(manuf) references Device(manufacturer)
+    foreign key(start, end) references Period(start, end),
+    foreign key(snum, manuf) references Device(serialnum, manufacturer),
     foreign key(pan) references PAN(domain));
---
+++ part2/table data.sql
@@ -0,0 +1,3 @@
+insert into Patient values('12345678', 'Roberto', 'Da Street, n.º3, 1ºEsq, 1000-999 Lisboa');
+insert into Patient values('98765432', 'Tóni', 'Da Street, n.º2, 4ºEsq, 1300-200 Oeiras');
+insert into Patient values('987654322', 'Tóni', 'Da Street, n.º2, 4ºEsq, 1300-200 Oeiras');
