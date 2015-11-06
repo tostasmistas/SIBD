add18cade2895e5fa5a52d5313887adac014bb0d
SophieTheSofa <sofia.fidalgo.da.silva@ist.utl.pt>
2015-11-06 15:53:16 +0000

    More inserts

--- part2/table creation.sql
+++ part2/table creation.sql
@@ -48,14 +48,14 @@ create table Municipality
     primary key(nut4code));
 
 create table Period
-   (start	DATETIME,
-    end		DATETIME,
-    primary key(start, end));
+   (start	TIMESTAMP,
+    ends		TIMESTAMP,
+    primary key(start, ends));
 
 create table Reading
    (snum		  NUMERIC(8,0),
     manuf     VARCHAR(255),
-    datetime  DATETIME,
+    datetime  TIMESTAMP,
     value     NUMERIC(5,2),  -- values read from the sensors have 5 total digits and 2 fractional digits
     primary key(snum, manuf, datetime),
     foreign key(snum, manuf) references Sensor(snum, manuf));
@@ -63,38 +63,38 @@ create table Reading
 create table Setting
    (snum		  NUMERIC(8,0),
     manuf		  VARCHAR(255),
-    datetime  DATETIME,
+    datetime  TIMESTAMP,
     value		  NUMERIC(5,2), -- settings sent to the actuators have 5 total digits and 2 fractional digits
     primary key(snum, manuf, datetime),
     foreign key(snum, manuf) references Actuator(snum, manuf));
 
 create table Wears
-   (start     DATETIME,
-    end    	  DATETIME,
+   (start     TIMESTAMP,
+    ends    	  TIMESTAMP,
     patient   NUMERIC(9,0),
     pan       VARCHAR(255),
-    primary key(start, end, patient),
-    foreign key(start, end) references Period(start, end),
+    primary key(start, ends, patient),
+    foreign key(start, ends) references Period(start, ends),
     foreign key(patient) references Patient(number),
     foreign key(pan) references PAN(domain));
 
 create table Lives
-   (start   DATETIME,
-    end    	DATETIME,
+   (start   TIMESTAMP,
+    ends    	TIMESTAMP,
     patient	NUMERIC(9,0),
     muni    NUMERIC(5,0),
-    primary key(start, end, patient),
-    foreign key(start, end) references Period(start, end),
+    primary key(start, ends, patient),
+    foreign key(start, ends) references Period(start, ends),
     foreign key(patient) references Patient(number),
     foreign key(muni) references Municipality(nut4code));
 
 create table Connects
-   (start   DATETIME,
-    end    	DATETIME,
+   (start   TIMESTAMP,
+    ends    	TIMESTAMP,
     snum	  NUMERIC(8,0),
     manuf   VARCHAR(255),
     pan     VARCHAR(255),
-    primary key(start, end, snum, manuf),
-    foreign key(start, end) references Period(start, end),
+    primary key(start, ends, snum, manuf),
+    foreign key(start, ends) references Period(start, ends),
     foreign key(snum, manuf) references Device(serialnum, manufacturer),
     foreign key(pan) references PAN(domain));
--- part2/table data.sql
+++ part2/table data.sql
@@ -1,13 +1,40 @@
+
 insert into Patient values('12345678', 'Roberto', 'Da Street, n3, 1Esq, 1000-999 Lisboa');
 insert into Patient values('98765432', 'Toni', 'Da Street, n2, 4Esq, 1300-200 Oeiras');
-insert into Patient values('987654322', 'Toni', 'Da Street, n2, 4Esq, 1300-200 Oeiras');  -- should fail
+insert into Patient values('987654322', 'Toni', 'Da Street, n2, 4Esq, 1300-200 Oeiras');
+
 
 insert into PAN values('pan99.healthunit.org', '914457220');
 insert into PAN values('pan33.healthunit.org', '845 763 207');
-insert into PAN values('pan99.healthunit.org', '+351 914457220'); -- should fail
+insert into PAN values('pan99.healthunit.org', '+351 914457220');
+
 
 insert into Device values('87650943', 'Philips', 'blood pressure');
 insert into Device values('98765432', 'Philips', 'blood pressure');
 insert into Device values('12345678', 'Philips', 'scale');
 insert into Device values('12098765', 'Philips', 'scale');
-insert into Device values('87650943', 'Philips', 'scale'); -- should fail
+insert into Device values('87650943', 'Philips', 'scale');
+insert into Device values('35647828', 'Philips', 'rotating speed');
+
+
+insert into Sensor values('87650943', 'Philips', 'mm Hg');
+insert into Sensor values('98765432', 'Philips', 'mm Hg');
+insert into Sensor values('12345678', 'Philips', 'kg');
+insert into Sensor values('12098765', 'Philips', 'kg');
+
+
+insert into Actuator values('35647828', 'Philips', 'rpm');
+
+
+insert into Municipality values('12345', 'Lisboa');
+insert into Municipality values('24351', 'Oeiras');
+insert into Municipality values('35374','Porto');
+
+
+insert into Period values('2015-12-13 17:50:44','2999-12-31 23:59:59');
+insert into Period values('2015-11-01 12:30:24','2999-12-31 23:59:59');
+insert into Period values('2014-05-29 08:56:21','2999-12-31 23:59:59');
+
+
+insert into Reading values('87650943', 'Philips', '2015-10-09 08:00:12', '60.3');
+insert into Reading values('87650944', 'Philips', '2015-10-09 08:00:12', '60.3');
