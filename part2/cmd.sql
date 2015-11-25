3a99ff4563290164f51d5323a988193d77c74fb2
Margarida Reis <margarida.reis@ist.utl.pt>
2015-11-25 18:54:24 +0000

    comand file to run all .sql files at once

--- part2/cmd.sql
+++
@@ -1,5 +0,0 @@
-use ist173099;
-source table creation.sql
-source check data.sql
-source overlap update test.sql
-source table data.sql
---
+++ part2/final files/cmd.sql
@@ -0,0 +1,5 @@
+use ist173099;
+source table_creation.sql
+source check_data.sql
+source overlap_periods.sql
+source table_data.sql
