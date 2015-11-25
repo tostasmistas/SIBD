76579296f03d10d9c08a9f7c604ec1b020e5cab7
SophieTheSofa <sofia.fidalgo.da.silva@ist.utl.pt>
2015-11-25 01:32:59 +0000

    time in 3c

--- part2/3c.sql
+++ part2/3c.sql
@@ -9,4 +9,13 @@ AND NOT EXISTS (SELECT nut4code
                                       AND w.pan = c.pan
                                       AND c.manuf = d1.manufacturer
                                       AND c.snum = d1.serialnum
+                                      AND TIMESTAMPDIFF(YEAR,l.end, NOW()) < 1
+                                      AND TIMESTAMPDIFF(YEAR,w.end, NOW()) < 1
+                                      AND TIMESTAMPDIFF(YEAR,c.end, NOW()) < 1
+                                      AND TIMESTAMPDIFF(SECOND,c.end, l.start) >= 0
+                                      AND TIMESTAMPDIFF(SECOND,l.end, c.start) >= 0
+                                      AND TIMESTAMPDIFF(SECOND,l.end, w.start) >= 0
+                                      AND TIMESTAMPDIFF(SECOND,w.end, l.start) >= 0
+                                      AND TIMESTAMPDIFF(SECOND,w.end, c.start) >= 0
+                                      AND TIMESTAMPDIFF(SECOND,c.end, w.start) >= 0
                                       AND d.manufacturer = d1.manufacturer));
